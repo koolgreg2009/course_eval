0e6f3e6d1d46dd0e6056fff70357988cdaa68455
kevinhu <kaiyuan.hu@mail.utoronto.ca>
2025-07-04 13:34:08 -0400

    adjusting settings for dev

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,12 @@ import dotenv from 'dotenv';
 import courseRoutes from './routes/courseRoutes';
 import professorRoutes from './routes/profRoutes';
 import evalRoutes from "./routes/evalRoutes";
+import path from 'path';
+import { fileURLToPath } from 'url'; // __dirname isnt defined by default since using ES module
+
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -23,6 +28,14 @@ app.get('/', (_req, res) => {
     res.send('Welcome to the Course Evaluation API');
 });
 
+app.use(express.static(path.join(__dirname, '../../frontend/dist'))); // serves all static assets (JS, CSS, images)
+
+app.get('*', (_req, res) => {
+    res.sendFile(path.join(__dirname, '../../frontend/dist', 'index.html')); // app.get('*') catch-all returns index.html for React Router
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
